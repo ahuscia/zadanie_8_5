b74a98934d91ae7d48be09a2170c1ae9a2050ad2
ahuscia <a.huscia@gmail.com>
2018-02-06 10:49:08 +0100

    Change in script.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,6 @@ var a = prompt("Base: ");
 var h = prompt("Height: ");
 var triangleArea = (a*h)/2;
 
-alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var message = 'Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea
+alert(message);
+console.log(message);
